21a7b9388f6bb68335da08f8007c0e84b78b896b
eeshwar das <iswardasa@gmail.com>
2016-01-28 14:41:16 -0800

    early prodramming - starting on keepKala Method

---
+++ arjuna-0.0.2/Arjuna.java
@@ -0,0 +1,17 @@
+/**
+*
+*Arjuna is the conqueror of sleep.
+*@version 0.0.2
+*@author eeshwar das
+*
+*/
+
+public class Arjuna
+{
+	public static void main( String[] args )
+	{
+		System.out.println(
+			"aum namo Arjunaya namaha" );
+	}
+}
+
